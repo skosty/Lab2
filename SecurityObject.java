3de7bec79ad74c821824db8207846c9bb7989fd7
skosty <skosty@utexas.edu>
2015-09-02 15:57:42 -0500

    Update SecurityObject.java

--- SecurityObject.java
+++ SecurityObject.java
@@ -1,13 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-/**
- *
- * 
- */
 public class SecurityObject {
   
   String name = null;
