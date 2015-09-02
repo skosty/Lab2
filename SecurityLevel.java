38d1afa21b6f49a9d170170c3781ad8155ffdfd3
skosty <skosty@utexas.edu>
2015-09-02 15:57:23 -0500

    Update SecurityLevel.java

--- SecurityLevel.java
+++ SecurityLevel.java
@@ -1,13 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-/**
- *
- * 
- */
 public class SecurityLevel {
   
   static final SecurityLevel HIGH = new SecurityLevel();
@@ -21,7 +11,5 @@ public class SecurityLevel {
     if(a == HIGH && b == LOW) return true; 
     else return false; 
   } // end dominates 
-  
-  
   
 }
